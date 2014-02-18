2ae4d183647d276c3f6c9c820f5e9850d503c487
Josh Charles <josh.charles@gmail.com>
2014-02-18 09:41:27 -0800

    Bam!  Mono 3.2 will now get installed.

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,60 +1,61 @@
 include apt
 
-class meta-social {
+class basicPackages {
+
+	exec{ "apt-update":
+		command => "/usr/bin/apt-get update",
+		before => Package['python-software-properties'],
+	}
+
+	package {"python-software-properties":
+		ensure => present,
+		before => Package["software-properties-common"]
+	}
+
+	package {"software-properties-common":
+		ensure => present,
+		before => Apt::Ppa["ppa:directhex/monoxide"],
+	}
 
 	package { "make":
 		ensure => present,
-#		before => Exec['/vagrant/scripts/build-mono.sh'],
 	}
 
 	package { "libtool":
 		ensure => present,
-#		before => Exec['/vagrant/scripts/build-mono.sh'],
 	}
 
 	package { "automake":
 		ensure => present,
-#		before => Exec['/vagrant/scripts/build-mono.sh'],
 	}
 
 	package { "autoconf":
 		ensure => present,
-#		before => Exec['/vagrant/scripts/build-mono.sh'],
 	}
 
 	package { "git-core":
 		ensure => present,
-#		before => Exec['/vagrant/scripts/build-mono.sh'],
 	}
 
 	package { "g++":
 		ensure => present,
-#		before => Exec['/vagrant/scripts/build-mono.sh'],
 	}
 
 	package { "gettext":
 		ensure => present,
-#		before => Exec['/vagrant/scripts/build-mono.sh'],
 	}
 
 	package { "pkg-config":
 		ensure => present,
-#		before => Exec['/vagrant/scripts/build-mono.sh'],
 	}
 
-	#apt::source { "badgerports":
-	#	location => "http://badgerports.org",
-	#	repos => "main",
-#		key => "0E1FAD0C",
-#		key_server => "keyserver.ubuntu.com",
-#		before => Exec['/vagrant/scripts/build-mono.sh'],
-#	}
+	apt::ppa { 'ppa:directhex/monoxide':
+		before => Package["mono-complete"],
+	}
 
-#	exec { "/vagrant/scripts/build-mono.sh":
-#		cwd => "/tmp",
-#		#creates => "/usr/local/bin/mono",
-#		timeout => 0,
-#	}
+	package{"mono-complete":
+		ensure => present,
+	}
 }
 
-include meta-social
+include basicPackages
